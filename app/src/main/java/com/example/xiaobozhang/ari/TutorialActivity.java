b11bc6968f2cc8f1de4024b512360e1e98931233
xiaobozhang <zhangxiaobo199102@gmail.com>
2015-01-24 20:07:42 -0500

    5:28

--- app/src/main/java/com/example/xiaobozhang/ari/TutorialActivity.java
+++ app/src/main/java/com/example/xiaobozhang/ari/TutorialActivity.java
@@ -1,31 +1,24 @@
 package com.example.xiaobozhang.ari;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class settingActivity extends ActionBarActivity implements View.OnClickListener{
-
-    Button goHomeBtn;
+public class TutorialActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
-        goHomeBtn = (Button) findViewById(R.id.button_goHome);   //there is a mistake here, button_setting cannot be found
-        goHomeBtn.setOnClickListener(this);
+        setContentView(R.layout.activity_tutorial);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
         return true;
     }
 
@@ -42,20 +35,5 @@ public class settingActivity extends ActionBarActivity implements View.OnClickLi
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void buttonClick() {
-
-        startActivity(new Intent("android.intent.action.MAIN"));
-    }
-    @Override
-    public void onClick(View v) {
-        switch(v.getId())
-        {
-            case R.id.button_goHome:
-                buttonClick();
-                break;
-        }
-
     }
 }

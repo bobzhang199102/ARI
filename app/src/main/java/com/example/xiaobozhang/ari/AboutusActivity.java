97c99b8a80e343eca06ae61d94207fefec16feb4
xiaobozhang <zhangxiaobo199102@gmail.com>
2015-01-24 20:31:37 -0500

    5:28

--- app/src/main/java/com/example/xiaobozhang/ari/AboutusActivity.java
+++ app/src/main/java/com/example/xiaobozhang/ari/AboutusActivity.java
@@ -1,31 +1,24 @@
 package com.example.xiaobozhang.ari;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MenuActivity extends ActionBarActivity implements View.OnClickListener {
+public class AboutusActivity extends ActionBarActivity {
 
-    Button goTutorialBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
-        goTutorialBtn = (Button) findViewById(R.id.button_tutorial);
-        goTutorialBtn.setOnClickListener(this);
+        setContentView(R.layout.activity_aboutus);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_aboutus, menu);
         return true;
     }
 
@@ -42,20 +35,5 @@ public class MenuActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void buttonClick() {
-
-        startActivity(new Intent("android.intent.action.Tutorial"));
-    }
-    @Override
-    public void onClick(View v) {
-        switch(v.getId())
-        {
-            case R.id.button_tutorial:
-                buttonClick();
-                break;
-        }
-
     }
 }
--- app/src/main/java/com/example/xiaobozhang/ari/TutorialActivity.java
+++ app/src/main/java/com/example/xiaobozhang/ari/TutorialActivity.java
@@ -1,17 +1,23 @@
 package com.example.xiaobozhang.ari;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class TutorialActivity extends ActionBarActivity {
+public class TutorialActivity extends ActionBarActivity implements  View.OnClickListener{
 
+    Button watchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        watchBtn = (Button) findViewById(R.id.button_watchVideo);
+        watchBtn.setOnClickListener(this);
     }
 
 
@@ -35,5 +41,20 @@ public class TutorialActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void buttonClick() {
+
+        startActivity(new Intent("android.intent.action.YoutubeActivity"));
+    }
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.button_watchVideo:
+                buttonClick();
+                break;
+        }
+
     }
 }
